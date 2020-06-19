6ad56a11e480c27d751398c9221b63918a87d679
autowit-intern <66835749+autowit-intern@users.noreply.github.com>
2020-06-19 12:17:12 -0400

    Add files via upload

---
+++ Passing-credentials-without-prompt.ps1
@@ -0,0 +1,20 @@
+﻿# how to Pass Credentials silently to the -credential parameter in invoke-command
+
+# First you ,ust save youre password to your filesystem
+
+read-host -assecurestring | convertfrom-securestring | out-file C:\securestring.txt 
+
+# Second you can now remote into a server without being prompted for credentials
+
+$targetserver = "192.168.0.1"
+$user = user1
+$pass = Get-Content 'C:\mysecurestring.txt' | ConvertTo-SecureString
+$creds = new-object -typename System.Management.Automation.PSCredential
+         -argumentlist $user, $pass
+# This command below remotes you into the targetserver without a prompt for credentials
+
+Invoke-Command -ComputerName $TargetServer -Credential $creds -ScriptBlock {
+
+# you can now run commands you would like here
+}
+
---
+++ passing-variable-to-arguementlist.ps1
@@ -0,0 +1,11 @@
+﻿# how to pass variables to the -argumentlist
+
+$test1 = "testing"
+
+
+Invoke-Command -ComputerName $TargetServer -Credential $creds -ScriptBlock { param ($test2="onetwothree")
+
+write-host $test1
+Write-Host $test2
+
+} -ArgumentList $test1
